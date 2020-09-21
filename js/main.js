1d1845d7bf1d09d7dc9f78722e1f91c8ee9b2e2d
Diego <saavedrad22@mail.wlu.edu>
2020-09-21 10:36:36 -0400

    Change CSS

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var date = new Date()
+document.body.innerHTML = "<h1> Today is " + date + "</h1>" ;
+//Comment line
+/* Comment Chunk*/
+
+var arayTest;
+arayTest = new Array ['Zeta',20,true]
+console.log(arayTest);
+
+var arayTest2;
+
+arayTest2 = ['pepe','tito'];
+console.log(arayTest2);
