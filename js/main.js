584863890e92a4dd4eade7fa3da5241fec24c0ce
Diego <saavedrad22@mail.wlu.edu>
2020-10-17 17:18:13 -0400

    Done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,3 @@
-var date = new Date()
-document.body.innerHTML = "<h1> Today is " + date + "</h1>" ;
+
 //Comment line
 /* Comment Chunk*/
-
-var arayTest;
-arayTest = new Array ['Zeta',20,true]
-console.log(arayTest);
-
-var arayTest2;
-
-arayTest2 = ['pepe','tito'];
-console.log(arayTest2);
