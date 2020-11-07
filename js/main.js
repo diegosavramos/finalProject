5486637943d5f0b6c6f49e683ca17c4d8b722eb1
Diego <saavedrad22@mail.wlu.edu>
2020-11-07 15:59:18 -0500

    done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 
-//Comment line
-/* Comment Chunk*/
+//Affect Interactive Variables
+var interVar = document.getElementById ('display-3');
+interVar.classList.add('animate__animated', 'animate__backInRight', 'animate__slower');
+console.log("Animation");
